817b86dc2aa04cba6d92d3223e72dea24642c021
MuhammedSaad <muhammedsaad.dev@gmail.com>
2024-10-05 22:22:52 +0300

    + quran tab widget

--- lib/core/assets_manager.dart
+++ lib/core/assets_manager.dart
@@ -6,4 +6,5 @@ class AssetsManager {
   static const String hadithIcon = '${base}hadith_icon.png';
   static const String tasbehIcon = '${base}sebha_icon.png';
   static const String radioIcon = '${base}radio_icon.png';
+  static const String quranHeaderImage = '${base}quran_header_image.png';
 }
